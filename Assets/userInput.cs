3ccc196cf24c9c22de69d11e940eee9e8e0c1768
MaxPower <89873437+MaxPowah@users.noreply.github.com>
2023-01-13 11:01:17 +0100

    sword added

---
+++ Assets/userInput.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class userInput : MonoBehaviour
+{
+    public GameObject sword;
+    Rigidbody2D selectedObject;
+    Vector3 mousePos;
+    Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void Update() {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Collider2D targetObject = Physics2D.OverlapPoint(mousePos);
+            if(targetObject){
+                selectedObject = sword.transform.gameObject.GetComponent<Rigidbody2D>();
+                offset = sword.transform.position - mousePos;
+
+            }
+        }
+        if (Input.GetMouseButtonUp(0) && selectedObject)
+        {
+            selectedObject = null;
+        }
+    }
+    void FixedUpdate()
+{
+    if (selectedObject)
+    {
+        selectedObject.MovePosition(mousePos + offset);
+    }
+}
+}
